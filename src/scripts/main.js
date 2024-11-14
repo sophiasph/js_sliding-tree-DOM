9eb481a9b5d65211a05d1ea25f9949ba81732257
Sophia <sophia13gal@gmail.com>
2024-11-14 13:44:14 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('li');
+
+for (const li of liElements) {
+  if (li.querySelector('ul')) {
+    const textNode = li.childNodes[0];
+    const correctText = textNode.textContent.trim();
+    const span = document.createElement('SPAN');
+
+    span.textContent = correctText;
+
+    li.replaceChild(span, textNode);
+  }
+}
+
+const tree = document.querySelector('.tree');
+
+// eslint-disable-next-line no-shadow
+tree.addEventListener('click', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    // eslint-disable-next-line no-unused-expressions
+    event.target.nextSibling.style.display === 'none'
+      ? (event.target.nextSibling.style.display = 'block')
+      : (event.target.nextSibling.style.display = 'none');
+  }
+});
